436cbf0620e0cda6f0e570f7c28c0f6b80aba883
TejpalSandhu <tejpal.s.sandhu007@gmail.com>
2026-02-04 18:40:28 +0530

    fix inngest import error for deployement in server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
-import { serve } from 'inngest/express'
+import { serve } from 'inngest/express';
+import { inngest, functions } from "./lib/inngest.js";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 

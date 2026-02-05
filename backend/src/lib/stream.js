cfbf1c0641883289b063f0b2ddb1e192147d7e1b
TejpalSandhu <tejpal.s.sandhu007@gmail.com>
2026-02-05 18:03:18 +0530

    fixed import error in inngest.js and console.log error in User.js

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,4 +1,4 @@
-import { StreamChat } from 'stream-chat';
+import { StreamChat } from "stream-chat";
 import { ENV } from "./env.js"
 
 const apiKey = ENV.STREAM_API_KEY
@@ -13,7 +13,7 @@ export const chatClient = StreamChat.getInstance(apiKey, apiSecret)
 export const upsertStreamUser = async (userData) => {
     try {
         await chatClient.upsertUser(userData)
-        console.error("Stream user upserted successfully:", userData)
+        console.log("Stream user upserted successfully:", userData)
     } catch (error) {
         console.error("Error upserting Stream user:", error)
     }
@@ -22,7 +22,7 @@ export const upsertStreamUser = async (userData) => {
 export const deleteStreamUser = async (userId) => {
     try {
         await chatClient.deleteUser(userId)
-        console.error("Stream user deleted successfully", userId)
+        console.log("Stream user deleted successfully", userId)
     } catch (error) {
         console.error("Error deleting the Stream user:", error)
     }
